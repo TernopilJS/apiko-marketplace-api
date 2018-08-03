91724a66336835d4ae853db41e165fd15c0eff99
Terry Sahaidak <s_tasssik_taras@ukr.net>
2018-08-03 17:44:02 +0300

    Add production database

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-// Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
 
@@ -39,17 +39,6 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'perun-site',
-      user:     'postgres',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    connection: process.env.DATABASE_URL + '?ssl=true',
   }
 };
