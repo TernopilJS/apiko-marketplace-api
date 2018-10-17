e4286328d6cb9356cf2d9b6c982bf6093ec72a9e
Terry Sahaidak <s_tasssik_taras@ukr.net>
2018-10-17 14:01:50 +0300

    Add user auth

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-import express from 'express';
+import express, { Response, Request } from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
 import router from './routes/router';
 
 type Port = string | number;
 
-const PORT: Port = process.env.PORT || 3000;
+const PORT: Port = process.env.PORT || 3111;
 
 const app: express.Application = express();
 app.use(bodyParser.json());
@@ -15,6 +15,12 @@ app.use(bodyParser.json());
 app.use('/api/v1', router);
 
 app.use('/apidoc', express.static(path.resolve(__dirname, 'doc')));
+
+app.use((err: Error, req: Request, res: Response, next: Function) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).send('invalid token...');
+  }
+});
 
 app.listen(PORT, (err: any) => {
   if (err) {
--- routes/authRoute.ts
+++ routes/authRoute.ts
@@ -1,6 +1,8 @@
 import { Response, Request } from 'express';
 import bcrypt from 'bcrypt';
+import jwt from 'jwt-simple';
 import * as usersDb from '../db/usersDb';
+import { JWT_SECRET } from '../constants';
 
 export type LoginParams = {
   email: string,
@@ -11,11 +13,21 @@ export async function login(req: Request, res: Response) {
   try {
     const params: LoginParams = req.body;
 
-    const [user]: usersDb.User[] = await usersDb.findUser(params as usersDb.FindUserParams);
+    const [user]: usersDb.User[] = await usersDb.findUser(
+      params as usersDb.FindUserParams,
+    );
+
+    if (!user) {
+      throw new Error('Wrong password or email');
+    }
 
     await bcrypt.compare(params.password, user.password);
 
-    res.json({ success: true });
+    const timestamp = new Date().getTime();
+    // TODO: Create token blacklist
+    const token = jwt.encode({ timestamp, id: user.id }, JWT_SECRET);
+
+    res.json({ token, user });
   } catch (err) {
     console.error(err.message);
 
@@ -66,6 +78,7 @@ export async function rememberPassword(req: Request, res: Response) {
       params,
     );
 
+    // TODO: Send email with reset password details
     res.json({ success: true });
   } catch (err) {
     console.error(err.message);
